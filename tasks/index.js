a897f52f3a1d2c96b5286c6417acd8d8194f56ab
James Cameron <jdonaldcameron@gmail.com>
2020-02-19 22:02:24 +1000

    refactor morningMeme to grab from /r/dankmemes hot posts

--- tasks/index.js
+++ tasks/index.js
@@ -5,6 +5,9 @@ const got = require("got");
 const { memeMessages } = require("../data/quotes");
 const { randomNumber } = require("../functions");
 
+// Store the URL so it doesn't post the same meme
+const postedMemeUrls = [];
+
 const morningMeme = async client => {
   // Grab the first text channel available
   const { id: channelId } = client.channels
@@ -12,16 +15,46 @@ const morningMeme = async client => {
     .first();
   const channel = client.channels.get(channelId);
 
-  // Grab a meme from /r/dankmemes
-  const embed = new RichEmbed();
-  const response = await got("https://www.reddit.com/r/dankmemes/random/.json");
-  const result = JSON.parse(response.body);
-  const image = result[0].data.children[0].data.url;
-  embed.setImage(image);
+  try {
+    // Grab a list of current hot memes
+    const embed = new RichEmbed();
+    const response = await got("https://www.reddit.com/r/dankmemes/hot/.json");
+    const result = JSON.parse(response.body);
+    const redditPosts = result.data.children;
 
-  // Grab a random good morning message
-  embed.setTitle(memeMessages[randomNumber(memeMessages.length)]);
-  channel.send(embed);
+    // Loop through the posts and find one that is an image and hasn't been posted previously
+    const image = redditPosts.find(post => {
+      const postUrl = post.data.url;
+      const isImage = postUrl.includes(".jpg") || postUrl.includes(".png");
+      const isNew = postedMemeUrls.every(url => url !== postUrl);
+      return isImage && isNew;
+    });
+
+    if (image) {
+      // We store seven images - the hot posts should have refreshed after a week
+      // Once we hit seven - remove them all and start again
+      if (postedMemeUrls.length > 6) {
+        postedMemeUrls.splice(0, postedMemeUrls.length);
+      };
+
+      // Add the new meme to postedMemeUrls array
+      const newMemeUrl = image.data.url;
+      postedMemeUrls.push(newMemeUrl);
+
+      // Send it to the channel
+      embed.setImage(newMemeUrl);
+      embed.setTitle(memeMessages[randomNumber(memeMessages.length)]);
+      channel.send(embed);
+    } else {
+      // There were no posts in the hot post list that haven't been posted before
+      channel.send("No good memes today folks. I'll be back... tomorrow");
+    }
+  } catch (error) {
+    console.info("morningMeme error: ", error);
+    channel.send(
+      "Sorry guys! There was an error retrieving your morning meme..."
+    );
+  }
 };
 
 module.exports = client => {
