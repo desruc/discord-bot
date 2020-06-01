b86bfd8bdd754788cd428b2ad2966e76f609e1c8
James Cameron <jdonaldcameron@gmail.com>
2020-06-01 22:12:34 +1000

    feat(joke command): convert joke into an embed

--- src/commands/memes/joke.ts
+++ src/commands/memes/joke.ts
@@ -1,8 +1,8 @@
-import { Client, Message } from 'discord.js';
+import { Client, Message, MessageEmbed } from 'discord.js';
 import Command from '../../core/command';
 import axios from 'axios';
 
-export default class Noot extends Command {
+export default class Joke extends Command {
   constructor() {
     super();
     this.name = 'joke';
@@ -10,23 +10,24 @@ export default class Noot extends Command {
   }
 
   private async getDadJoke(): Promise<string> {
-    try {
-      const {
-        data: { joke }
-      } = await axios.get('https://icanhazdadjoke.com/', {
-        headers: {
-          'User-Agent': 'Discord Bot (https://github.com/desruc/discord-bot)',
-          Accept: 'application/json'
-        }
-      });
-      return joke;
-    } catch {
-      return 'Apologies. I\'m all out of jokes right now.';
-    }
+    const {
+      data: { joke }
+    } = await axios.get('https://icanhazdadjoke.com/', {
+      headers: {
+        'User-Agent': 'Discord Bot (https://github.com/desruc/discord-bot)',
+        Accept: 'application/json'
+      }
+    });
+    return joke;
   }
 
   public async exec(client: Client, message: Message): Promise<Message> {
-    const joke = await this.getDadJoke();
-    return message.channel.send(joke);
+    try {
+      const joke = await this.getDadJoke();
+      const embed = new MessageEmbed().setTitle(joke).setColor('RANDOM');
+      return message.channel.send(embed);
+    } catch {
+      return message.channel.send("Apologies. I'm all out of jokes right now.");
+    }
   }
 }
