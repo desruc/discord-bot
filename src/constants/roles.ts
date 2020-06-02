966ffac8aedaaa2cd3867c3a7c66b1881e82e08d
James Cameron <jdonaldcameron@gmail.com>
2020-06-03 08:22:55 +1000

    feat: add command to give base level role to all guild members

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -2,6 +2,37 @@ interface NewRoleMeta {
   name: string;
   level?: number;
   position?: number;
+  color?: string;
 }
 
-export const roles: NewRoleMeta[] = [];
+export const roles: NewRoleMeta[] = [
+  {
+    name: 'Villager',
+    level: 0,
+    position: 100,
+    color: '#07a3d7'
+  }
+];
+
+/*
+Dungeon Master
+Dragonborn
+Dwarf
+Elf
+Gnome
+Half-Elf
+Halfling
+Half-Orc
+Human
+Tiefling
+Goliath
+Goblin
+Hobgoblin
+Kobold
+Orc
+Minotaur
+Centaur
+Locathah
+Firbolg
+Bugbear
+*/
