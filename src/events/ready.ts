6661188876db80107cc68641f8a953d67d05afb0
James Cameron <jdonaldcameron@gmail.com>
2020-06-07 16:37:25 +1000

    feat(sharding): add sharding

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,5 @@
 import Bot from '../core/bot';
+import { Presence } from 'discord.js';
 import { IEvent } from '../typings';
 
 export default class ReadyEvent implements IEvent {
@@ -7,8 +8,28 @@ export default class ReadyEvent implements IEvent {
   constructor(private client: Bot) {}
 
   public exec(): void {
+    const numOfUsers = this.client.users.cache.filter(
+      (u) => !u.equals(this.client.user!)
+    ).size;
+    const numOfGuilds = this.client.guilds.cache.size;
+
+    const logMessage = `${numOfUsers} users on ${numOfGuilds} guilds!`;
+
     this.client.logger.info(
-      `Logged in as ${this.client.user.tag} and ready to serve!`
+      `${
+        this.client.shard ? `[Shard #${this.client.shard.ids}]` : ''
+      } I'm ready to serve ${logMessage}`
     );
+
+    const updatePresence = async (): Promise<Presence> =>
+      this.client.user!.setPresence({
+        activity: {
+          name: `${await this.client.getUsersCount()} users!`,
+          type: 'WATCHING'
+        }
+      });
+
+    setInterval(updatePresence, 30 * 1000);
+    updatePresence();
   }
 }
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -6,6 +6,7 @@ export interface BotConfig {
   name: string;
   prefix: string;
   modRole: string;
+  totalShards: string;
 }
 
 export interface ICommand {
